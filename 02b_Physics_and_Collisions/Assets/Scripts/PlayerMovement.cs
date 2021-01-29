0904816fbfee6213f924ec9b5641803a121caa3f
randyfortier <randy.fortier@ontariotechu.net>
2021-01-29 15:37:28 -0500

    Added sprite animation demo

--- 02b_Physics_and_Collisions/Assets/Scripts/PlayerMovement.cs
+++ 02b_Physics_and_Collisions/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerMovement : MonoBehaviour {
     [SerializeField] private float movementSpeed = 5.0f;
     [SerializeField] private float jumpForce = 400f;
@@ -13,13 +14,16 @@ public class PlayerMovement : MonoBehaviour {
     [SerializeField] private float closeEnough = 0.1f;
 
     private Rigidbody2D rb;
+    private Animator controller;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        controller = GetComponent<Animator>();
     }
 
     private void Update() {
         float movementX = Input.GetAxis("Horizontal") * movementSpeed;
+        Debug.Log(movementX);
 
         RaycastHit2D hit = Physics2D.Raycast(footPosition.position, Vector2.down, closeEnough, groundLayers);
 
@@ -31,9 +35,16 @@ public class PlayerMovement : MonoBehaviour {
 
         if (Input.GetButtonDown("Jump") && isGrounded) {
             rb.AddForce(Vector3.up * jumpForce);
+            controller.SetTrigger("IsJumping");
         }
 
         Vector3 movement = Vector3.right * movementX * Time.deltaTime;
         transform.Translate(movement);
+        controller.SetFloat("Speed", Mathf.Abs(movementX));
+        if (movementX < 0f) {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        } else {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 }
